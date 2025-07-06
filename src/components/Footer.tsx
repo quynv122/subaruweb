dc9641f743bc54e4d9dfe52bbcc57b0898c98730
Quy <quynv.122@gmail.com>
2025-07-06 19:05:33 +0700

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Footer: React.FC = () => {
+
+const Footer = () => {
+  const navigate = useNavigate();
   return (
-    <footer className="bg-gray-800 text-white py-6 text-center">
-      <p>&copy; 2025 Tên Sản Phẩm. All rights reserved.</p>
+    <footer className="border-t py-4 text-center text-sm text-gray-700 bg-white">
+      <div className="flex justify-center gap-4 mb-1 text-[13px] font-medium text-gray-900">
+        <button  className="hover:underline">GIỚI THIỆU</button>
+        <button  className="hover:underline">CHÍNH SÁCH BẢO MẬT</button>
+        <button onClick={() => navigate("/Contact")} className="hover:underline">LIÊN HỆ</button>
+      </div>
+      <div className="text-xs text-gray-600">
+        Copyright 2024 © Subaru Hà Nội
+      </div>
     </footer>
   );
 };
